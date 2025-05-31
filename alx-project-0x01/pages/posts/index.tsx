2f4dd4c5469978c72b725a18eee96af2228aa545
DevOsi123 <gatelogistics@consultant.com>
2025-05-31 08:32:14 -0700

    new code

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,36 +1,39 @@
-import { GetStaticProps } from 'next';
-import { useState } from 'react';
-import PostCard from '../../components/posts/PostCard';
-import Modal from '../../components/common/Modal';
-import { fetchPosts } from '../../utils/fetchPosts';
-import { Post } from '../../interfaces/Post';
+import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = await fetchPosts();
-  return { props: { posts: posts.slice(0, 5) } };
-};
-
-export default function PostsPage({ posts }: { posts: Post[] }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+  console.log(posts)
   return (
-    <div className="p-6 space-y-4">
-      <h1 className="text-2xl font-bold">Posts</h1>
-
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="bg-blue-600 text-white px-4 py-2 rounded"
-      >
-        Add Post
-      </button>
-
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <p>This is where your post form will go!</p>
-      </Modal>
-
-      {posts.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
+        </div>
+      </main>
     </div>
-  );
+  )
 }
+
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Posts;
